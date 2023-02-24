bc1ee44360822704dae57a512e9108ed2d96e985
Augusto Cristofoli <106482872+AugustoEXE@users.noreply.github.com>
2023-02-24 09:40:22 -0300

    Initial commit

---
+++ testetwig.php
@@ -0,0 +1,12 @@
+<?php
+require('vendor/autoload.php');
+
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+$twig = new \Twig\Environment($loader);
+
+$template = $twig->load('teste.html');
+echo $template->render([
+    'nome'=> 'augusto',
+    'idade' => 999,
+    'title' => 'bom dia'
+]);
