747c822cc5f222e7b54b0abce0a58b42532d23fa
Augusto Cristofoli <106482872+AugustoEXE@users.noreply.github.com>
2023-02-24 11:08:57 -0300

    adicionado lista de itens

--- produtos.php
+++ produtos.php
@@ -4,7 +4,27 @@ require('vendor/autoload.php');
 $loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($loader);
 
+$produtos = [
+    [
+        'nome' => 'chinelo',
+        'preco' => 30,
+    ],
+    [
+        'nome' => 'camiseta',
+        'preco' => 50,
+    ],
+    [
+        'nome' => 'carro',  
+        'preco' => 999,
+    ],
+    [
+        'nome' => 'avião',  
+        'preco' => 100000000,
+    ],
+];
+
 $template = $twig->load("produtos.html");
 echo $template->render([
     'title'=> 'produtos',
+    'produtos' => $produtos,
 ]);
