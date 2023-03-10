65dc479e77143f21daa9faaae30229047f3ca4ad
Augusto Cristofoli <106482872+AugustoEXE@users.noreply.github.com>
2023-03-10 09:51:10 -0300

    branches

--- loginVerifica.php
+++ loginVerifica.php
@@ -3,20 +3,25 @@ require("inc/pdo.inc.php");
 $user = $_POST['login'];
 $pass = $_POST['pass'];
 
-$sql = $pdo->prepare('SELECT * FROM usuarios WHERE username = :usr AND senha= :pass');
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE username = :usr');
 
 $sql->bindParam(':usr', $user);
-$sql->bindParam(':pass', $pass);
 
-$sql->execute();    
+
+$sql->execute();
 
 if ($sql->rowCount()) {
     $user = $sql->fetch(PDO::FETCH_OBJ);
 
+    if (!password_verify($pass, $user->senha)) {
+        header('location:login.php?erro=1');
+        die;
+    }
+
     session_start();
     $_SESSION['user'] = $user->nome;
     header('location:boasVindas.php');
-    
+
     die;
 } else {
     echo 'foi de base';
